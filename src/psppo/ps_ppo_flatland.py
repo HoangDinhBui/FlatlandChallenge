54da7d862d86dbe8eb57169bee28b5390f493924
HoangBD <buidinhhoang1910@gmail.com>
2026-02-23 21:49:38 +0700

    update: code save model after train

--- src/psppo/ps_ppo_flatland.py
+++ src/psppo/ps_ppo_flatland.py
@@ -240,7 +240,7 @@ def train_multiple_agents(env_params, train_params):
             ppo.reset_stats()
 
     # Save model sau khi train xong
-    ppo.save_policy("gnn_model.pt")
+    ppo.save("gnn_model.pt")
     print("Model saved to gnn_model.pt")
 
     return env.env.accumulated_normalized_score, \
